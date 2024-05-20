ce37b482273cae1b5fc5dcab7fbb708bbd997450
Brennan <blfische92@gmail.com>
2024-05-20 19:33:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Please input phrase to count the number of spaces");
+            string readResult = Console.ReadLine();
+            Console.WriteLine("Please input phrase to count the number of spaces");
+            char charToCount = Convert.ToChar(Console.ReadLine());
+            foreach (char c in readResult)
+            {
+                if(charToCount == c)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("There are " + count + " instances of your character");
+        }
+    }
+}
